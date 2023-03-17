04d3898e5018bbbc1b629436c6505e643ff1aff2
Erin Timlin <erintimlin@gmail.com>
2023-03-17 13:53:31 -0400

    temporarily comment out required message

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -5,7 +5,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-    [Required(ErrorMessage = "Name is required")]
+    //[Required(ErrorMessage = "An engineer must be added")]
     public string MachineName { get; set; }
     public List<Engineer> Engineers { get; set; }
     public List<EngineerMachine> EngineerMachines { get; }
