55c9a0457e4d0e45d42a88b357b8b0d828805e18
Erin Timlin <erintimlin@gmail.com>
2023-03-17 11:39:08 -0400

    add engineer class

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Engingeer
+  {
+    public int EngineerId { get; set; }
+    [Required(ErrorMessage = "An engineer must be added")]
+    public string Name { get; set; }
+    public int MachineId { get; set; }
+    public Machine Machine { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
