02c8d29a18aba0184a225cdf2293ded437cac9bc
Erin Timlin <erintimlin@gmail.com>
2023-03-19 15:23:56 -0400

    add hiredate migration

---
+++ Factory/Migrations/20230319191118_AddHireDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddHireDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Engineers",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Engineers");
+        }
+    }
+}
