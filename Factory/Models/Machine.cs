2a92ed4150a972b1b480a3f1414c37ed021d65a0
Erin Timlin <erintimlin@gmail.com>
2023-03-19 12:42:33 -0400

    update validation method for machines and engineers

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,7 +6,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-    [Required(ErrorMessage = "An engineer must be added")]
+    [Required(ErrorMessage = "A machine must be added")]
     public string MachineName { get; set; }
     //public List<Engineer> Engineers { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
